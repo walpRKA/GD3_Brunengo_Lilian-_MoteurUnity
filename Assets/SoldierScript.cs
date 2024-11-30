f367dc3c6f5d42a876bcb08350bf78591af3703c
walpRKA <145676508+walpRKA@users.noreply.github.com>
2024-11-30 18:21:38 +0100

    animations & read me

--- Assets/SoldierScript.cs
+++ Assets/SoldierScript.cs
@@ -2,28 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class SoldierScript : MonoBehaviour
 {
     Animator animator;
     float speed = 0.0f;
     float attack = 0.0f;
+    float Idle = 0.0f;
+    bool SwitchIdle = true;
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        animator = GetComponent<Animator>();
 
-
-    }
 
     // Update is called once per frame
+
     void Update()
     {
+        animator = GetComponent<Animator>();
+        
+
+
         if (Input.GetKey(KeyCode.I))
         {
-            Debug.Log("OOOOH");
+
             speed += Time.deltaTime;
 
         }
@@ -33,18 +36,51 @@ public class SoldierScript : MonoBehaviour
             speed = 0.0f;
         }
 
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
+            StartCoroutine(Jump());
+        }
 
-            attack += 1;
+
+        IEnumerator Jump()
+        {
+            attack = 1;
+            yield return new WaitForSeconds(1.2f);
+            attack = 0;
 
         }
 
-        if (Input.GetKeyUp(KeyCode.E))
-        {
-            attack -= 1;
-        }
+
+
         animator.SetFloat("runParameter", speed);
-        animator.SetFloat("Punch", attack);
+        animator.SetFloat("Jump", attack);
+        animator.SetFloat("IdleType", Idle);
+
+
+
+
+
+
+    }
+    void Start()
+    {
+        InvokeRepeating(nameof(ChangeIdle), 0f, 2f);
+        
+    }
+    void ChangeIdle()
+    {
+        if (SwitchIdle == true)
+        {
+            Idle = 0;
+            SwitchIdle = false;
+
+        }
+        else
+
+        { 
+            Idle = 1;
+            SwitchIdle = true;
+        }
+
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # GD3_Brunengo_Lilian-_MoteurUnity
-Chaque joueur possède 4 tuiles de mah-jong différentes, il doit trouver les paires qui correspondent aux tuiles qu’il a déjà. Lorsqu’il fait un perd, il récupère les tuiles et construit sa main. 
-Le premier joueur à compléter sa main gagne.
- Les joueurs n’ont pas le droit de compléter leurs mains avec des tuiles de l’adversaire, révèler les tuiles de l’adversaire leur donne un avantage. 
+
+
+Appuyer sur espacer pour sauter.
+
+Notes : 
+J'ai tenté de refaire toutes les animations dans un blend tree pour que ce soit plus simple agréable à manipuler.
+ 
+Known bugs : 
+Les animations ne se reset pas et semblent loop malgré tout. Je n'ai pas réussi à fixer ce bug jusqu'à maintenant.
+
+
+Question : 
+
+J'ai retenu que les modèles et les animations utilisent les mêmes assets peu importe le moteur. Il est possible de refaire ce que j'ai fait 
+sur unreal avec les mêmes assets pris de mixamo par exemple. D'une manière similaire, les FSMs existent dans différents moteurs de jeu mais
+l'interface et les manières de les utiliser varient.
+
+Si j'arrive dans un studio disposant d'un moteur que je connais pas et que je dois intégrer le travail des graphistes : j'ai besoin des modèles,
+des textures, et des animations squelettiques. Tout cela est généralement contenu dans un FBX. Je dois simplement demander à un autre employé qui connait déjà le moteur
+comment ouvrir l'animator et comment l'appeler sur son personnage. Intégrer et configurer les FSMs ne devrait pas être très différent.
